a113d52aad6b155e65fe0fa76c26e920ab23adb9
Detective-Chaff <toddoneil42@gmail.com>
2025-02-08 12:02:10 -0500

    - added main file - added unit test file - added main and testing shell scripts

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from textnode import TextNode
+
+
+print("hello world")
+
+def main():
+    node = TextNode("this is a text node", "bold", "https://www.boot.dev")
+    print(node)
+
+if __name__ == "__main__":
+    main()
+
---
+++ src/textnode.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALICS = "italics"
+    CODE = "code"
+    LINKS = "link"
+    IMAGES = "image"
+
+class TextNode():
+    def __init__(self, text, type, url = None):
+        self.text = text
+        self.text_type = type
+        self.url = url
+    
+    def __eq__(self, other):
+        if isinstance(other, TextNode):
+            return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return False
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
